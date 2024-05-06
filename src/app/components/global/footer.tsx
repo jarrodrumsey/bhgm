8b1295146f3f6847e77a7958c112e94ec42eb484
Jarrod Rumsey <jrumsey@mun.ca>
2024-05-06 00:46:13 -0230

    Additional contact for footer

--- src/app/components/global/footer.tsx
+++ src/app/components/global/footer.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const Footer = () => {
-    return <div className="w-full h-16 p-2 bg-sky-950 flex flex-col justify-center items-center">
-      <p className='text-xs'>© {new Date().getFullYear()} Jarrod Rumsey | All rights reserved</p>
+    return <div className="w-full h-16 p-2 bg-sky-950 flex flex-col gap-1 justify-center items-center text-xs">
+      <p className=''>© {new Date().getFullYear()} Jarrod Rumsey | All rights reserved</p>
+      <div className='flex gap-2'>
+        <a href='https://linkedin.com/in/jarrodrumsey'>Website</a>
+        <a href='https://github.com/jarrodrumsey'>GitHub</a>
+        <a href='https://linkedin.com/in/jarrodrumsey'>LinkedIn</a>
+      </div>
     </div>
 }
 
