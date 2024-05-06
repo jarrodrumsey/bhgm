6bdca0c7c7b484f32b85ed23106ccf54e3eadd51
Jarrod Rumsey <jrumsey@mun.ca>
2024-05-06 00:53:19 -0230

    underline for links in footer

--- src/app/components/global/footer.tsx
+++ src/app/components/global/footer.tsx
@@ -4,9 +4,9 @@ const Footer = () => {
     return <div className="w-full h-16 p-2 bg-sky-950 flex flex-col gap-1 justify-center items-center text-xs">
       <p className=''>© {new Date().getFullYear()} Jarrod Rumsey | All rights reserved</p>
       <div className='flex gap-2'>
-        <a href='https://linkedin.com/in/jarrodrumsey'>Website</a>
-        <a href='https://github.com/jarrodrumsey'>GitHub</a>
-        <a href='https://linkedin.com/in/jarrodrumsey'>LinkedIn</a>
+        <a className='underline' href='https://jarrodrumsey.ca/' target='_blank'>Website</a>
+        <a className='underline' href='https://github.com/jarrodrumsey' target='_blank'>GitHub</a>
+        <a className='underline' href='https://linkedin.com/in/jarrodrumsey' target='_blank'>LinkedIn</a>
       </div>
     </div>
 }
