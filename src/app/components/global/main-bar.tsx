07ef4845d4cc7c2139e2ba42889d701e97b48702
Jarrod Rumsey <jrumsey@mun.ca>
2024-05-06 01:22:44 -0230

    ssr again

--- src/app/components/global/main-bar.tsx
+++ src/app/components/global/main-bar.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ProgressBar from '../progress-bar'
 import dynamic from 'next/dynamic'
-import ProgressInfo, { CountDownItemProps } from '../progress-info'
-
+import { CountDownItemProps } from '../progress-info'
+const ProgressInfoNOSSR = dynamic(() => import('../progress-info'), { ssr: false })
+ 
 const MainBar = (props:{progressValue?:number, max?:number, countdowns:CountDownItemProps[]}) => {
   return (
     <div className="w-full sticky top-0 border-b-[1px] border-slate-800 bg-slate-900/30 z-40  backdrop-blur-lg 
     flex p-4 flex-col gap-2 ">
         {/*<button className="bg-green-400 rounded-md p-2" onClick={() => {incrementIndex()}}>Test</button>*/}
         <ProgressBar value={props.progressValue ?? 1} max={props.max ?? 1} color="bg-gradient-to-r from-red-500 to-yellow-500" background="bg-slate-400">
-            <ProgressInfo countdowns={props.countdowns} />
+            <ProgressInfoNOSSR countdowns={props.countdowns} />
         </ProgressBar>
     </div>
   )
